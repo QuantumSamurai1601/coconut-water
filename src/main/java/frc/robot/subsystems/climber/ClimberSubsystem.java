8323a29d49b8a3c4abfaa41a0e0f2a6e7384329e
Victor Tan <shenlain.nyc@gmail.com>
2024-03-02 11:48:34 -0500

    Climber

--- src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -1,8 +1,32 @@
 package frc.robot.subsystems.climber;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import static frc.robot.Constants.*;
 
 public class ClimberSubsystem extends SubsystemBase {
-    
+    private final TalonFX leftClimber;
+    private final TalonFX rightClimber;
+    private final DutyCycleOut leftRequest;
+    private final DutyCycleOut rightRequest;
+
+    public ClimberSubsystem() {
+        leftClimber = new TalonFX(LEFT_CLIMBER_ID, CANBUS_NAME);
+        rightClimber = new TalonFX(RIGHT_CLIMBER_ID, CANBUS_NAME);
+
+        leftClimber.setNeutralMode(NeutralModeValue.Brake);
+        rightClimber.setNeutralMode(NeutralModeValue.Brake);
+
+        leftRequest = new DutyCycleOut(0.0, true, false, false, false);
+        rightRequest = new DutyCycleOut(0.0, true, false, false, false);
+    }
+
+    public void climb(double speed) {
+        leftClimber.setControl(leftRequest.withOutput(speed));
+        rightClimber.setControl(rightRequest.withOutput(speed));
+    }
 }
