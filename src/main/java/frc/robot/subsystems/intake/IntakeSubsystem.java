07e1298b1b2d233e7babc6e2937becf5abc75a18
Victor Tan <shenlain.nyc@gmail.com>
2024-03-03 13:38:31 -0500

    Fix incorrect motor controller

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -12,16 +12,16 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import static frc.robot.Constants.*;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private final VictorSPX leftIntake;
-    private final VictorSPX rightIntake;
+    private final TalonSRX leftIntake;
+    private final TalonSRX rightIntake;
     private final TalonFX leftIntakePivot;
     private final TalonFX rightIntakePivot;
     private final DutyCycleOut leftPivotRequest;
     private final Follower rightPivotRequest;
 
     public IntakeSubsystem() {
-        leftIntake = new VictorSPX(LEFT_INTAKE_ID);
-        rightIntake = new VictorSPX(RIGHT_INTAKE_ID);
+        leftIntake = new TalonSRX(LEFT_INTAKE_ID);
+        rightIntake = new TalonSRX(RIGHT_INTAKE_ID);
         leftIntakePivot = new TalonFX(LEFT_INTAKE_PIVOT_ID, CANBUS_NAME);
         rightIntakePivot = new TalonFX(RIGHT_INTAKE_PIVOT_ID, CANBUS_NAME);
         leftPivotRequest = new DutyCycleOut(0.0, true, false, false, false);
@@ -30,14 +30,14 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public Command chomp() {
         return this.runOnce(() -> {
-            leftIntake.set(ControlMode.PercentOutput, 1);
+            leftIntake.set(ControlMode.PercentOutput, 0.5);
             rightIntake.set(ControlMode.Follower, LEFT_INTAKE_ID);
         });
     }
 
     public Command vomit() {
         return this.runOnce(() -> {
-            leftIntake.set(ControlMode.PercentOutput, -1);
+            leftIntake.set(ControlMode.PercentOutput, -0.5);
             rightIntake.set(ControlMode.Follower, LEFT_INTAKE_ID);
         });
     }
@@ -49,10 +49,10 @@ public class IntakeSubsystem extends SubsystemBase {
         });
     }
 
-    public Command pivot(double speed) {
-        return this.run(() -> {
-            leftIntakePivot.setControl(leftPivotRequest.withOutput(speed));
-            rightIntakePivot.setControl(rightPivotRequest);
-        });
-    }
+    //public Command pivot(double speed) {
+    //    return this.runOnce(() -> {
+    //        leftIntakePivot.setControl(leftPivotRequest.withOutput(speed));
+    //        rightIntakePivot.setControl(rightPivotRequest);
+    //    });
+    //}
 }
